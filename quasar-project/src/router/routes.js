8534b43f103fbe8d218598a583a95430044adfea
victorsantanna <victor.esef@hotmail.com>
2024-07-05 11:14:38 -0300

    feat: adiciona as rotas para pages

--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -3,7 +3,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'portfolionutri', component: () => import('pages/PortfolioNutri.vue') },
+      { path: 'testeimc', component: () => import('pages/ImcTeste.vue') }
     ]
   },
 
@@ -12,7 +14,8 @@ const routes = [
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
-  }
+  },
+
 ]
 
 export default routes
