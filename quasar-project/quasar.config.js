f76eda6648f9a4600a32264b5601b484ecbcb461
victorsantanna <victor.esef@hotmail.com>
2024-07-08 10:33:05 -0300

    feat: adiciona o vue-elemente loading

--- quasar-project/quasar.config.js
+++ quasar-project/quasar.config.js
@@ -21,8 +21,8 @@ module.exports = configure(function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
     boot: [
-      
-      
+      'vue-element-loading', 
+
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
