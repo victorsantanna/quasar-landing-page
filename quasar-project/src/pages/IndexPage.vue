0f1640032e61d18763e2850a8ff2a22df0a725d0
victorsantanna <victor.esef@hotmail.com>
2024-07-08 10:35:30 -0300

    style: adiciona estilização

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -2,20 +2,24 @@
   <q-page class="flex flex-center">
     <q-page-container>
       <div class="custom-gutter">
-        <div class="q-col-gutter-md" style="display: flex;">
-
+        <div class="q-col-gutter-md" style="display: flex; align-items: center;">
           <div class="q-col" style="padding: 20px;">
-            <div  class="content-wrapper">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in est mi. Aliquam faucibus rhoncus
-                vehicula. Aenean non hendrerit lectus, at porta nibh. Nullam justo diam, blandit vitae rutrum ac, pulvinar
-                id mi. Quisque at lectus in elit lobortis faucibus venenatis ut mauris. Fusce at iaculis lacus, at aliquam
-                ex. Pellentesque malesuada cursus libero, vel pulvinar nulla rutrum vel.
+            <div class="content-wrapper">
+              <p class="text-body1 text-weight-medium  q-pb-xl" shadow-2>
+                Olá! Eu sou a Luiza Souza, nutricionista dedicada a ajudar você a alcançar um estilo de vida mais
+                saudável e equilibrado.<br> Com anos de experiência na área de nutrição, meu objetivo é proporcionar a
+                você
+                um atendimento personalizado e de qualidade, focado nas suas necessidades individuais.
+                <span class="gradient-border"></span>
               </p>
             </div>
           </div>
-          <div class="q-col" style="padding: 20px;">
-
-            <img class="img-home" src="../assets/img/foto-nutri.png" alt="foto do  nutricionista">
+          <div class="q-col img-container" style="padding: 20px;">
+            <q-img class="img-home" src="../assets/img/foto-nutri.png" alt="foto do  nutricionista">
+              <div class="absolute-bottom text-h6 text-white">
+                Luiza Souza
+              </div>
+            </q-img>
 
           </div>
         </div>
@@ -33,19 +37,37 @@ export default defineComponent({
 </script>
 
 <style>
-
 .custom-gutter {
-  margin-left: auto; /* Para alinhar à direita */
-  margin-right: auto; /* Para alinhar à esquerda */
-  max-width:75%; /* 80% da largura disponível */
-  padding-left: 10%; /* Espaço à esquerda */
-  padding-right: 10%; /* Espaço à direita */
-  padding-bottom: 10%;
+  margin-left: auto;
+
+  margin-right: auto;
+
+  max-width: 75%;
+
+  padding-left: 10%;
+
+  padding-right: 10%;
+
 }
 
-.img-home{
-  width: 200px;
+.img-home {
+  width: 250px;
   border-radius: 40px 10px 40px 10px;
-  height: 400px;
+  height: 350px;
+}
+
+
+
+.gradient-border {
+  display: inline-block;
+  width: 350px;
+  height: 15px;
+  margin-bottom: 30px;
+  border: none;
+  background: linear-gradient(to right, #ffffff, #26A69A);
+  border-image: linear-gradient(to right, #ffffff, #26A69A);
+  border-image-slice: 1;
+  border-radius: 80px;
+  margin-top: 20px;
 }
 </style>
