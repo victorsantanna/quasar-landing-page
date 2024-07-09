8a0445631cddb49cafaaec3cf3825b00a20f8be1
victorsantanna <victor.esef@hotmail.com>
2024-07-09 11:49:40 -0300

    refactor: retira o q-page-container

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,30 +1,28 @@
 <template>
   <q-page class="flex flex-center">
-    <q-page-container>
-      <div class="custom-gutter">
-        <div class="q-col-gutter-md" style="display: flex; align-items: center;">
-          <div class="q-col" style="padding: 20px;">
-            <div class="content-wrapper">
-              <p class="text-body1 text-weight-medium  q-pb-xl" shadow-2>
-                Olá! Eu sou a Luiza Souza, nutricionista dedicada a ajudar você a alcançar um estilo de vida mais
-                saudável e equilibrado.<br> Com anos de experiência na área de nutrição, meu objetivo é proporcionar a
-                você
-                um atendimento personalizado e de qualidade, focado nas suas necessidades individuais.
-                <span class="gradient-border"></span>
-              </p>
-            </div>
-          </div>
-          <div class="q-col img-container" style="padding: 20px;">
-            <q-img class="img-home" src="../assets/img/foto-nutri.png" alt="foto do  nutricionista">
-              <div class="absolute-bottom text-h6 text-white">
-                Luiza Souza
-              </div>
-            </q-img>
-
+    <div class="custom-gutter">
+      <div class="q-col-gutter-md" style="display: flex; align-items: center;">
+        <div class="q-col" style="padding: 20px;">
+          <div class="content-wrapper">
+            <p class="text-body1 text-weight-medium  q-pb-xl" shadow-2>
+              Olá! Eu sou a Luiza Souza, nutricionista dedicada a ajudar você a alcançar um estilo de vida mais
+              saudável e equilibrado.<br> Com anos de experiência na área de nutrição, meu objetivo é proporcionar a
+              você
+              um atendimento personalizado e de qualidade, focado nas suas necessidades individuais.
+              <span class="gradient-border"></span>
+            </p>
           </div>
         </div>
+        <div class="q-col img-container" style="padding: 20px;">
+          <q-img class="img-home" src="../assets/img/foto-nutri.png" alt="foto do  nutricionista">
+            <div class="absolute-bottom text-h6 text-white">
+              Luiza Souza
+            </div>
+          </q-img>
+
+        </div>
       </div>
-    </q-page-container>
+    </div>
   </q-page>
 </template>
 
